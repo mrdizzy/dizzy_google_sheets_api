0e73f7c0423785e14a2fa5860d6932af2a60f855
EC2 Default User <ec2-user@ip-172-31-46-56.eu-west-1.compute.internal>
2019-12-11 15:00:56 +0000

    Added promise chaining test

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,7 @@
 const functions = require('firebase-functions');
 const { google } = require("googleapis");
+const spreadsheetId = "1H2-Bo6LhYNEs4zUVmLkbH3F0t1mPoCpZMBA1DBap7A8";
 let credentials = require("./credentials.json");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
 
 const jwt = new google.auth.JWT(credentials.client_email, null, credentials.private_key, "https://www.googleapis.com/auth/spreadsheets")
 
@@ -13,6 +10,26 @@ const sheets = google.sheets({
     auth: jwt
 });
 
+/*********************************
+ ** Write data to a single cell
+ **********************************/
+function updateCells(spreadsheetId, range, arrayOfArrayValues) {
+
+    return new Promise((resolve, reject) => {
+        sheets.spreadsheets.values.update({
+            spreadsheetId: spreadsheetId,
+            range: range,
+            valueInputOption: "USER_ENTERED",
+            resource: {
+                values: arrayOfArrayValues
+            }
+        }, (error, response) => {
+            if (error) { reject(new Error("The api threw an error" + error)) }
+            resolve(response);
+        })
+    })
+
+}
 
 
 // range needs to be in format "A1:A500"
@@ -41,13 +58,25 @@ function appendValuesToSheet(spreadsheetId, range, arrayOfArrayValues) {
 // Seecond argument to .then is a function that runs when the promise is rejected
 
 exports.testPromises = functions.https.onRequest((request, response) => {
-    appendValuesToSheet("1uFsDidKOKf8NBrUtX_ZnHn2zvVGbxDxkU0z259MCAP4", "A1:D500", [
-        ["David", "21", "Mexico"],
-        ["Margaret", "38", "New York"],
-        ["Janice", "87", "Oklahoma"]
-    ]).then(responseJson => {
-        response.send(200)
+
+    updateCells(spreadsheetId, "Inventory!G5", [
+        ["Aidan"]
+    ]).then(function(response) {
+        updateCells(spreadsheetId, "Inventory!H5", [
+            ["Light"]
+        ])
+    }).then(function(response) {
+        updateCells(spreadsheetId, "Inventory!H7", [
+            ["David"]
+        ])
     })
+    // appendValuesToSheet(spreadsheetId, "A1:D500", [
+    //     ["David", "21", "Mexico"],
+    //     ["Margaret", "38", "New York"],
+    //     ["Janice", "87", "Oklahoma"]
+    // ]).then(responseJson => {
+    //     response.send(200)
+    // })
 
 })
 
